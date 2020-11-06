c1b46b7f6df52ac35b8746efa09bd77873325bdc
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2020-11-06 23:56:35 +0500

    asd

--- src/Components/privateRoute.js
+++ src/Components/privateRoute.js
@@ -2,16 +2,32 @@ import React from 'react';
 import { Navigate, Route } from 'react-router';
 import { AuthContext } from './authContext'
 
-function PrivateRoute({ component : RouteComponent, ...rest }) {
+function PrivateRoute({ element : RouteComponent, ...rest }) {
     const { currentUser } = React.useContext(AuthContext)
     console.log("PrivateRoute / CurrentUser --> ", currentUser)
-    console.log("PrivateRoute / ...rest --> ", ...rest)
+    console.log("PrivateRoute / ...rest --> ", rest)
+    console.log("PrivateRoute / RouteComponent --> ", RouteComponent)
     return (
         <Route
         {...rest}
-        render={routeProps => !!currentUser ? ( <Navigate to="/login" />  ) : ( <RouteComponent {...routeProps} /> )
+        render={routeProps => !!currentUser ?  ( <RouteComponent {...routeProps} /> ) : ( <Navigate to="/login" />  )
         
         }
+        // render={
+        //     (routeProps) => 
+        //     {
+        //         console.log("Current user render props ")
+        //     if(currentUser)
+        //     {
+        //         console.log("Current user is not null")
+        //         return( <RouteComponent {...routeProps} /> )
+        //     }
+        //     else
+        //     {
+        //         console.log("Current user is null")
+        //         return( <Navigate to="login" /> )
+        //     }
+        // }}
         
         />
     );
